bb44b09dba52fdeed4a4d4b33b674c4066dc06a4
ktw0133 <136439186+ktw0133@users.noreply.github.com>
2024-07-13 16:43:03 +0900

    몬스터 기능 추가
    Monster Script, HpBar, Monster Animation(Idle, Hit Death)

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Monster : MonoBehaviour
 {
+    [SerializeField] private HpBar hpBar;
+    [SerializeField] private Text nameText;
+
+    [SerializeField] private string monsterName;
     [SerializeField] private float maxHp;
     private float curHp;
 
     private bool isDead = false;
+    private Animator animator;
 
     private void Awake()
     {
         curHp = maxHp;
+        animator = GetComponent<Animator>();
+        nameText.text = monsterName;
     }
 
     public void OnHit(float damage)
@@ -22,11 +30,14 @@ public class Monster : MonoBehaviour
             curHp = 0;
             isDead = true;
         }
+        animator.SetTrigger("Hit");
         Debug.Log("Slime Hit!, Current Hp : " + curHp);
+        hpBar.ChangeHpBarAmount(curHp / maxHp);
 
         if (isDead)
         {
             Debug.Log("Slime is Dead");
+            animator.SetTrigger("Death");
             Destroy(gameObject, 1.5f);
         }
     }
